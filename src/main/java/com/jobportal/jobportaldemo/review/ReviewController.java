7bdb490a5ceb752c85fb5c2c656a080f97a1a160
MANAD DESAI <desaimanad123@gmail.com>
2024-05-06 11:01:51 -0400

    Containerized application

--- src/main/java/com/jobportal/jobportaldemo/review/ReviewController.java
+++ src/main/java/com/jobportal/jobportaldemo/review/ReviewController.java
@@ -3,10 +3,13 @@ package com.jobportal.jobportaldemo.review;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -50,6 +53,27 @@ public class ReviewController {
 			return new ResponseEntity<Review>(getReview, HttpStatus.OK);
 		}
 		return new ResponseEntity<Review>(HttpStatus.NOT_FOUND);
+	}
+	@PutMapping("/reviews/{reviewId}")
+	public ResponseEntity<String> updateReview(@PathVariable Long companyId, @PathVariable Long reviewId, @RequestBody Review review)
+	{
+		boolean isUpdated = reviewService.updateReview(companyId, reviewId, review);
+		if(isUpdated)
+			return new ResponseEntity<String>("Review Updates Successfully", HttpStatus.OK);
+		else
+			return new ResponseEntity<String>("Review Not found", HttpStatus.NOT_FOUND);
+	}
+	
+	@DeleteMapping("/reviews/{reviewId}")
+	public ResponseEntity<String> deleteReview(@PathVariable Long companyId, @PathVariable Long reviewId)
+	{
+		boolean isDeleted = reviewService.deleteReview(companyId, reviewId);
+		
+		if(isDeleted)
+		{
+			return new ResponseEntity<>("Reciew Deleted successfuk", HttpStatus.OK);
+		}
+		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 	}
 
 }
--- src/main/java/com/jobportal/jobportaldemo/review/ReviewImpl.java
+++ src/main/java/com/jobportal/jobportaldemo/review/ReviewImpl.java
@@ -40,22 +40,45 @@ public class ReviewImpl implements ReviewService {
 		{
 			return false;
 		}
-			
-		
-		
 	}
 
 
 	@Override
 	public Review getReview(Long companyId, Long reviewId) {
 		// TODO Auto-generated method stub
-		Company company = companyService.getCompany(companyId);
-		if(company != null)
-		{
-			Review review = reviewRespository.findById(reviewId).get();
-			return review;
-		}
-		return null;
+		List<Review> reviews = reviewRespository.findByCompanyId(companyId);
+		
+		return reviews.stream().filter(review -> review.getId().equals(reviewId)).findFirst().orElse(null);
+		
 	}
+
+
+	@Override
+	public boolean updateReview(Long companyId, Long reviewId, Review newreview) {
+		if(companyService.getCompany(companyId) != null)
+		{
+			newreview.setCompany(companyService.getCompany(companyId));
+			newreview.setId(reviewId);
+			reviewRespository.save(newreview);
+			return true;
+		}
+		return false;
+	}
+
+
+	@Override
+	public boolean deleteReview(Long companyId, Long reviewId) {
+		// TODO Auto-generated method stub
+		if(companyService.getCompany(companyId) != null && reviewRespository.existsById(reviewId) 
+				&& reviewRespository.findById(reviewId).get().getCompany().getId() == companyId)
+		{
+			Review deleteReview = reviewRespository.findById(reviewId).get();
+			reviewRespository.delete(deleteReview);
+			return true;
+			
+		}
+		return false;
+	}
+	
 
 }
